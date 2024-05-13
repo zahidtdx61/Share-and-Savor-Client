aafde3138d6694e474d4f477a7663def1649cc9e
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-14 00:50:00 +0600

    added featured foods section

--- src/components/FeaturedFoods/FeaturedFoods.jsx
+++ src/components/FeaturedFoods/FeaturedFoods.jsx
@@ -1,33 +1,64 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import useSession from "../../hooks/useSession";
+import { Link } from "react-router-dom";
 import { ScaleLoader } from "react-spinners";
+import useSession from "../../hooks/useSession";
+import FoodCard from "../FoodCard/FoodCard";
 
 const FeaturedFoods = () => {
   const queryClient = useQueryClient();
   const session = useSession();
 
-  const { data, isLoading, isError, error } = useQuery({
+  const {
+    data: foods = [],
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["featuredFoods"],
     queryFn: () => getFoodsData(),
   });
 
   const getFoodsData = async () => {
     const response = await session("/all-foods?sorted=quantity&page=1&size=6");
-    return response.data;
+    return response.data.foods;
   };
 
-  console.log({ data, isLoading, isError, error });
+  console.log({ foods, isLoading, isError, error });
 
   if (isLoading)
     return (
       <div
-        className={`w-[95%] min-h-[calc(100vh-400px)] lg:max-w-screen-xl mx-auto   rounded-lg  mt-12 mb-8 p-2 md:p-4 lg:p-10  flex flex-col  justify-center  items-center `}
+        className={`min-h-[calc(100vh-400px)] lg:max-w-screen-xl mx-auto  flex flex-col  justify-center  items-center `}
       >
         <ScaleLoader size={40} color="#1ba94c" />
       </div>
     );
 
-  return <div></div>;
+  return (
+    <div className="w-full mt-12">
+      <h2 className="text-4xl font-mulish font-bold text-center">
+        Featured Foods
+      </h2>
+      <p className="text-center text-slate-500 mt-4 lg:w-[70%] mx-auto my-4">
+        Introducing our selection of dishes generously portioned to serve a
+        crowd. Whether you're hosting a party, celebration, or family gathering,
+        these crowd-pleasers are designed to satisfy and bring everyone
+        together. Discover the joy of sharing food with our selection that can
+        be enjoyed by many!
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {foods.map((food) => (
+          <FoodCard key={food._id} food={food} />
+        ))}
+      </div>
+
+      <div className="w-full">
+        <button className="bg-green-600 block text-white hover:bg-opacity-75 text-xl w-fit mx-auto px-5 py-2 rounded-md">
+          <Link to="/available-foods">View All Available Foods</Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedFoods;
---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,69 @@
+import { Divider, useColorScheme } from "@mui/joy";
+import PropTypes from "prop-types";
+import { CiLocationOn } from "react-icons/ci";
+import { FaMountain } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
+import { Link } from "react-router-dom";
+
+const FoodCard = ({ food }) => {
+  const { mode } = useColorScheme();
+  const {
+    food_name,
+    food_image,
+    quantity,
+    expiry_date,
+    location,
+    notes,
+    status,
+  } = food;
+  return (
+    <div
+      className={`shadow-lg ${
+        mode === "dark" && "bg-zinc-700"
+      }  rounded-md overflow-hidden w-full flex flex-col font-mulish`}
+    >
+      <div className="w-full h-[250px] relative">
+        <img
+          src={food_image}
+          className="w-full h-full object-cover hover:scale-105 duration-1000"
+        />
+        <div className="absolute top-3 -left-1 bg-zinc-200 text-slate-700 px-5 py-1 rounded">
+          {status}
+        </div>
+      </div>
+      <div>
+        <h2 className="text-lg font-bold p-2">{food_name}</h2>
+      </div>
+      <div className="flex-1 p-4">
+        <div className="flex gap-1 items-center">
+          <FaMountain />
+          <span>Quantity: </span>
+          <span>For {quantity} Person</span>
+        </div>
+        <div className="flex gap-1 items-center">
+          <SlCalender /> <span>Expiry Date: </span>
+          <span>{new Date(expiry_date).toLocaleDateString()}</span>
+        </div>
+        <div className="flex gap-1 items-center">
+          <CiLocationOn />
+          <span>Location: </span>
+          <span>{location}</span>
+        </div>
+      </div>
+      <Divider />
+      <div className="w-full p-4">
+        <button className="text-green-600 border border-green-600 hover:text-green-400 hover:border-green-400 px-5 py-2 w-full text-center">
+          <Link to={`/food/${food._id}`} className="w-full">
+            View Details
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+FoodCard.propTypes = {
+  food: PropTypes.object.isRequired,
+};
+
+export default FoodCard;
