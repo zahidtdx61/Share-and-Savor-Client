842ad088d29de2f2183bada8568b599e5781085d
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-05-13 10:23:41 +0600

    addded initials

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+const Home = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Share and Savor | Home</title>
+      </Helmet>
+      <div>Home</div>
+    </>
+  );
+};
+
+export default Home;
